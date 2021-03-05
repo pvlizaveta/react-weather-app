6f11a34d4cc66eccd78c433c794bf20545b169c1
pvlizaveta <pv.lizaveta@gmail.com>
2021-03-05 15:01:16 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        <a href="https://github.com/pvlizaveta/react-weather-app">
-          Open-source code
-        </a>
-        &nbsp; by Lizaveta Pauliushchyk
-      </footer>
+      <div class="container">
+        <h1>Weather app</h1>
+        <footer>
+          <a href="https://github.com/pvlizaveta/react-weather-app">
+            Open-source code
+          </a>
+          &nbsp; by Lizaveta Pauliushchyk
+        </footer>
+      </div>
     </div>
   );
 }

e3b8d9c7afcbead2dee8885bc0105ba561cb4c23
pvlizaveta <pv.lizaveta@gmail.com>
2021-03-05 13:58:58 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        <a href="https://github.com/pvlizaveta/react-weather-app">
+          Open-source code
+        </a>
+        &nbsp; by Lizaveta Pauliushchyk
+      </footer>
+    </div>
+  );
 }

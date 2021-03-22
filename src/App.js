97af4f984d2f8025a8139ba2017c823788cc6800
pvlizaveta <pv.lizaveta@gmail.com>
2021-03-22 14:21:37 -0400

    Css update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div class="container">
+      <div className="container">
         <Weather />
-
-        <footer>
-          <a href="https://github.com/pvlizaveta/react-weather-app">
-            Open-source code
-          </a>
-          &nbsp; by Lizaveta Pauliushchyk
-        </footer>
       </div>
+      <footer>
+        <a href="https://github.com/pvlizaveta/react-weather-app">
+          Open-source code
+        </a>
+        &nbsp; by Lizaveta Pauliushchyk
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -4,46 +4,61 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
+      <div className="row">
+        <div className="column left">
+          <ul className="Cities">
+            <li className="City">
+              <a href=" ">Tokyo</a>
+            </li>
+            <li className="City">
+              <a href=" ">Madrid</a>
+            </li>
+            <li className="City">
+              <a href=" ">New York</a>
+            </li>
+            <li className="City">
+              <a href=" ">San Francisco</a>
+            </li>
+          </ul>
+          <h1 className="mainCity">Charlotte</h1>
+          <h3 className="state">NC</h3>
+          <h1 className="temperature">17º</h1>
+          <h3 className="time">3:00pm</h3>
+          <h4>Friday, May 13,2021</h4>
+        </div>
+        <div class="vl"></div>
+        <div className="column right">
+          <img
+            className="sunny"
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Sunny
+            "
+          />
+          <h5 className="condition">Sunny</h5>
+          <hr></hr>
+          <div className="input-group">
             <input
               type="search"
-              placeholder="Enter a city"
-              className="form-control"
+              className="form-control rounded"
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="search-addon"
             />
+            <span className="input-group-text border-0" id="search-addon">
+              <i className="fas fa-search"></i>
+            </span>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="Temp">
+            <ul className="discription">
+              <li>Feels like: 20º</li>
+              <hr></hr>
+              <li>Humidity: 50%</li>
+              <hr></hr>
+              <li>Wind: 10km/h</li>
+            </ul>
           </div>
         </div>
-      </form>
-
-      <h1>Charlotte, NC</h1>
-      <h4>Friday 3:00pm</h4>
-      <h5>Sunny</h5>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="Sunny"
-          />
-          18º
-        </div>
       </div>
-      <div className="Temp">
-        <ul>
-          <li>Precipitation: 15%</li>
-          <li>Humidity: 50%</li>
-          <li>Wind: 10km/h</li>
-        </ul>
-      </div>
-      <ul className="Cities">
-        <li className="City">Tokyo</li>
-        <li className="City">Madrid</li>
-        <li className="City">New York</li>
-        <li className="City">San Francisco</li>
-      </ul>
     </div>
   );
 }

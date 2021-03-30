c924f1bbbb8f07c1f09e5409d33ffccacb30edcc
pvlizaveta <pv.lizaveta@gmail.com>
2021-03-30 13:21:15 -0400

    Use the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -15,6 +15,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       time: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       humidity: response.data.main.humidity,
       city: response.data.name,
       feelsLike: response.data.main.feels_like,
@@ -71,12 +72,7 @@ export default function Weather(props) {
           </div>
           <div className="vl"></div>
           <div className="column right">
-            <img
-              className="sunny"
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="Sunny
-            "
-            />
+            <img className="iconUrl" src={weatherData.iconUrl} alt="iconUrl" />
             <h5 className="text-capitalize">{weatherData.condition}</h5>
             <hr></hr>
             <div className="Forms">
